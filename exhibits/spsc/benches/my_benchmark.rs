bf925d762cb23a5a966d9119143918d2d53ec05e
zephaniahong <zealorez@gmail.com>
2026-08-21 16:19:15 +0800

    pin cores for spsc benchmark

--- exhibits/spsc/benches/my_benchmark.rs
+++ exhibits/spsc/benches/my_benchmark.rs
@@ -21,6 +21,7 @@ fn spsc_benchmark(c: &mut Criterion) {
 
             thread::scope(|s| {
                 let consumer = s.spawn(move || {
+                    core_affinity::set_for_current(core_affinity::CoreId { id: 3 });
                     barrier2.wait();
 
                     for _ in 0..iters {
@@ -30,6 +31,7 @@ fn spsc_benchmark(c: &mut Criterion) {
                     }
                 });
 
+                core_affinity::set_for_current(core_affinity::CoreId { id: 1 });
                 barrier.wait();
                 let start = Instant::now();
 
@@ -48,7 +50,7 @@ fn spsc_benchmark(c: &mut Criterion) {
 
     group.bench_function("spsc box", |b| {
         b.iter_custom(|iters| {
-            let mut spsc: SPSCBox<u64> = SPSCBox::new(128).unwrap();
+            let mut spsc: SPSCBox<usize> = SPSCBox::new(128).unwrap();
             let (mut writer, mut reader) = spsc.split();
 
             let barrier = Arc::new(Barrier::new(2));
@@ -56,6 +58,7 @@ fn spsc_benchmark(c: &mut Criterion) {
 
             thread::scope(|s| {
                 let consumer = s.spawn(move || {
+                    core_affinity::set_for_current(core_affinity::CoreId { id: 3 });
                     barrier2.wait();
 
                     for _ in 0..iters {
@@ -65,11 +68,12 @@ fn spsc_benchmark(c: &mut Criterion) {
                     }
                 });
 
+                core_affinity::set_for_current(core_affinity::CoreId { id: 1 });
                 barrier.wait();
                 let start = Instant::now();
 
                 for i in 0..iters {
-                    while writer.send(i).is_some() {
+                    while writer.send(i as usize).is_some() {
                         std::hint::spin_loop();
                     }
                 }
@@ -84,59 +88,3 @@ fn spsc_benchmark(c: &mut Criterion) {
 
 criterion_group!(benches, spsc_benchmark);
 criterion_main!(benches);
-
-// fn spsc_bench() {
-//     const N: usize = 100000;
-//     let mut spsc: SPSC_CG<usize, 128> = SPSC_CG::new().unwrap();
-//     let (mut writer, mut reader) = spsc.split();
-//
-//     thread::scope(|s| {
-//         s.spawn(|| {
-//             for i in 0..N {
-//                 while writer.send(i).is_some() {
-//                     std::hint::spin_loop();
-//                 }
-//             }
-//         });
-//
-//         for _ in 0..N {
-//             while reader.recv().is_none() {
-//                 std::hint::spin_loop();
-//             }
-//         }
-//     });
-// }
-
-// fn main() {
-//     let mut total = Duration::new(0, 0);
-//     const N: usize = 100000;
-//     for _ in 0..1000 {
-//         let mut spsc = SPSC::new(100);
-//         let (mut writer, mut reader) = spsc.split();
-//
-//         let start = Instant::now();
-//
-//         thread::scope(|s| {
-//             s.spawn(|| {
-//                 for i in 0..N {
-//                     while writer.send(i).is_some() {
-//                         std::hint::spin_loop();
-//                     }
-//                 }
-//             });
-//
-//             for _ in 0..N {
-//                 while reader.recv().is_none() {
-//                     std::hint::spin_loop();
-//                 }
-//             }
-//         });
-//
-//         let elapsed = start.elapsed();
-//         total += elapsed;
-//     }
-//
-//     let avg = total / 1000;
-//
-//     println!("Average: {:?}", avg);
-// }
